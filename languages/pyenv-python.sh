47f309b48f6532c69e4f23f5707dedaba9e6b65e
Lance7in <zexpp5@gmail.com>
2018-04-15 20:03:22 +0800

    fix typo

--- languages/pyenv-python.sh
+++ languages/pyenv-python.sh
@@ -28,4 +28,4 @@ pyenv install 3.6.5
 pyenv global 3.6.5  2.7.13
 pip install --no-cache-dir --upgrade pip
 pip install --no-cache-dir virtualenv
-echo "pyenv has been installed with python 3.5.1 and virtualenv"
+echo "pyenv has been installed with python 3.6.5 and virtualenv"
