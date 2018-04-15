74e39fa6365cd37bda62902ca4c972ba41bf2309
Lance7in <zexpp5@gmail.com>
2018-04-15 20:00:18 +0800

    fix typo
    fix typo

--- languages/java.sh
+++ languages/java.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "Installing and configuring Java 8..."
+echo "Installing and configuring Java 9..."
 add-apt-repository ppa:webupd8team/java
 apt-get update
 apt-get install -y oracle-java9-installer oracle-java9-set-default
